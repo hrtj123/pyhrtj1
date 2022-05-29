8566282c16d177c82e5bf43349b341c86cf3bd1d
Hrituraj Chakraborty <hrtj123@gmail.com>
2022-05-29 19:18:09 +0530

    changes

--- p12022/app/models.py
+++ p12022/app/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Data(models.Model):
     Name = models.CharField(max_length=100)
     salary = models.CharField(max_length=20)
+
--- p12022/app/views.py
+++ p12022/app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django import forms
 from django.views.decorators.http import require_safe
-
-from .models import Data
+from . models import Data
 
 
 class SalaryForm(forms.ModelForm):
@@ -11,8 +10,8 @@ class SalaryForm(forms.ModelForm):
         fields = "__all__"
 
 
-@require_safe
-def home(request):
+# @require_safe
+def home(request): # NOSONAR
     if request.method == 'POST':
         form = SalaryForm(request.POST)
         if form.is_valid():
